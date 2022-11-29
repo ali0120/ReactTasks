b957795f4c7f44e5bffff556a5abca315620e501
Ali Abdelrahman <aliabdelrahman848@gmail.com>
2022-11-29 18:08:16 +0200

    Update App.js
    review

--- project/src/App.js
+++ project/src/App.js
@@ -1,60 +1,57 @@
-import { Table, Button } from 'antd';
 import React from 'react';
+import { Table, Button } from 'antd';
 
-const data = [];
-function startWith() {
-   for (let i = 0; i < 10; i++){
-        data.push({
-                key: `${i}`,
-                name: `Student ${i}`,
-                age: `${i + 20}`
-              });
-  };
-};
+const data = [
+  { id: Math.trunc((Math.random() * 100) + 1), name: 'Student 1', age: '29' },
+  { id: Math.trunc((Math.random() * 100) + 1), name: 'Student 2', age: '29' },
+  { id: Math.trunc((Math.random() * 100 + 1)), name: 'Student 3', age: '29' },
+];
 
-function addStudent() {
-    data.push({
-      key: `10`,
-      name: `esraa`,
-      age: `64536`
-    });
+function App() {
+  const [dataSource, setDataSource] = React.useState(data);
+
+  function addStudent() {
+    setDataSource((prev) => [...prev, {
+      id: Math.floor(1 + Math.random() * 100),
+      name: `esraa ${Math.trunc(1 + Math.random() * 100)}`,
+      age: Math.trunc(1 + Math.random() * 100)
+    }])
   };
-function removeStudent() {
-    data.shift();
+
+  console.log(data);
+
+  function removeStudent(e) {
+    const filterData = dataSource.filter((item) => item.id !== e.id)
+    setDataSource(filterData);
   };
-  function App() {
-    const [dataSource, setDataSource] = React.useState([]);
-    React.useEffect(() => {
-      startWith();
-      setDataSource(data);
-    }, []);
+
   const columns = [
     {
       title: "Name",
-      dataIndex:"name",
+      dataIndex: "name",
     },
     {
       title: "Age",
-      dataIndex:"age",
+      dataIndex: "age",
     },
     {
       title: "Action",
       render: (_, record) => {
         return <>
-          <Button danger  type="primary" onClick={removeStudent()} >Delete</Button>
-          </>
+          <Button danger type="primary" onClick={() => removeStudent(record)} >Delete</Button>
+        </>
       },
     },
   ];
- 
+
   return (
     <div className="App">
-      
-        <br />
-        <Button type="primary" onClick={addStudent()} >Add</Button>
-        <br/>
-        <Table    id='Table' columns={columns} dataSource={dataSource}></Table>
-    {/* <button>Primary Button</button> */}
+
+      <br />
+      <Button type="primary" onClick={addStudent} >Add</Button>
+      <br />
+      <Table id='Table' columns={columns} dataSource={dataSource}></Table>
+      {/* <button>Primary Button</button> */}
 
 
     </div>
